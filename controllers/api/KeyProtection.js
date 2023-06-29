14c1041f35403023cebc0a9646e904dd3d4e1f19
Kenyon Gordon <kenyongordon@MacBook-Pro-2.local>
2023-06-28 23:25:02 -0500

    Update KeyProtection.js

--- controllers/api/KeyProtection.js
+++ controllers/api/KeyProtection.js
@@ -1,5 +1,8 @@
 const randomstring = require('randomstring');
+const sequelize = require('../../config/connection');
 
 // Generate a random string of a specified length
 const randomString = randomstring.generate(16); // 16 characters long
 console.log(randomString);
+
+module.exports = sequelize;
