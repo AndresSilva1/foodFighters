0cdc6ce214229270c4c082b55e1aefb184f6953d
Ralph Recinto <rdbz49ers@gmail.com>
2023-07-05 01:27:48 -0700

    newchanges

--- Server.js
+++ Server.js
@@ -13,28 +13,31 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3306;
 
-// const sessionStore = new SequelizeStore ({db: food_db})
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+});
+
 // Set up sessions
 const sess = {
-    secret: 'Super secret secret',
-    resave: false,
-    saveUninitialized: true,
-  };
-  
-  app.use(session(sess));
-  
-  const hbs = exphbs.create({});
-  
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
-  
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
-  
-  app.use(routes);
-  
-  sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
-  
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
