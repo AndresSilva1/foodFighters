ba7aa0400cfe96aca22c47cdbb9bff91a6317667
Hans <126028768+HansVanHousen@users.noreply.github.com>
2023-07-06 15:37:43 -0700

    changed capitialization, port numbers, html

--- Server.js
+++ Server.js
@@ -8,43 +8,45 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const { url } = require('inspector');
 // const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3306;
 
 //Data to connect API to server.js
-function getAPIUrl() {
-    let url = "https://controllerdata.lacity.org/resource/v2mg-qsxf.json";
-    return url;
-}
+// function getAPIUrl() {
+//     let url = "https://controllerdata.lacity.org/resource/v2mg-qsxf.json";
+//     return url;
+// }
 
-function fetchData(url) {
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            return data.data["results"];
-        })
-        .then(function (results) {
-            setHTML(results);
-        });
-}
+// function fetchData(url) {
+//     fetch(url)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             return data.data["results"];
+//         })
+//         .then(function (results) {
+//             setHTML(results);
+//         });
+// }
 
-function fetchSuggestions(url) {
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            return data.data["results"];
-        })
-        .then(function (results) {
-            suggestionData = results;
-        });
-}
-fetchData(getAPIUrl())
+// function fetchSuggestions(url) {
+//     fetch(url)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             return data.data["results"];
+//         })
+//         .then(function (results) {
+//             suggestionData = results;
+//         });
+// }
+// fetchData(getAPIUrl())
+// fetchSuggestions(url)
 //similar to a console.log
 const sessionStore = new SequelizeStore({
     db: sequelize,
